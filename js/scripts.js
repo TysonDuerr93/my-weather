847766e79fff4405ee79a30d03ab4213074f1ac8
TysonDuerr93 <tduerr@engr-131-17.eastern.ewu.edu>
2016-05-25 16:39:22 -0700

    Version1

--- js/scripts.js
+++ js/scripts.js
@@ -27,8 +27,25 @@ $.simpleWeather({
       // Display Data
       $('#cheney .city').text(weather.city);
       $('#cheney .temp').text(weather.temp);
-    
+      // Display icon  
+ $('#cheney i').addClass( 'icon-' + weather.code );    
 
+        
+        //Get forecast
+        $("#d1 .day").text(weather.forecast[1].date);
+        $("#d1 .temp").text(weather.forecast[1].high);
+    $('#di i').addClass( 'icon-' + weather.forecast[1].code);
+        
+        
+        //Add custom body class
+        if ( weather.code >= 26 && weather.code <= 30 ){
+                        $("body").removeClass();
+
+            $("body").addClass("bg-1");
+        
+        }
+
+        
     
       // Entire weather object
       console.log(weather);
@@ -50,11 +67,10 @@ $.simpleWeather({
       // Display Data
       $('#spokane .city').text(weather.city);
       $('#spokane .temp').text(weather.temp);
-      // Display icon  
- $('#cheney i').addClass( 'icon-' + weather.code );
-
-    
-      // Entire weather object
+      // Display icon
+     $('#spokane i').addClass( 'icon-' + weather.code );
+      
+        // Entire weather object
       console.log(weather);
     },
     error: function(error) {
@@ -62,5 +78,44 @@ $.simpleWeather({
     }
   
   });
+
+// Get Geo Location
+// Check for GeoLocation Support on Browser
+if ('geolocation' in navigator) {
+
+   $('.geo button').show(); 
+
+} else {
+  
+  $('.geo button').hide();
+  $('.geo').prepend('<p>Geolocation Not Supported</p>');
+
+}
+// Get geolocated weather
+var getWeather = function(location) {
+    
+   $.simpleWeather({
+    location: location,
+    woeid: '',
+    unit: 'f',
+    success: function(weather) {
+      
+      // Display Data
+      $('.geo .temp').text(weather.temp);
+      $('.geo .city').text(weather.city);
+    // Display Icon
+      $('.geo i').addClass( 'icon-' + weather.code );    
+        
+      // Entire weather object
+      console.log();
+    },
+    error: function(error) {
+      // Show if weather cannot be retreived
+    }
+  
+  });
+    
+};
+
 
 
